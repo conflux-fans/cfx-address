25e433de82a19e2a7962e4a1c9b627f125932d37
Zhang Wenda <wenda.zhang@confluxnetwork.org>
2022-08-19 11:29:27 +0800

    Bump version: 1.0.0-beta.3 → 1.0.0-beta.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = "Zhang Wenda"
 
 # The full version, including alpha/beta/rc tags
 # modify by bumpversion
-release = "1.0.0-beta.3"
+release = "1.0.0-beta.4"
 
 
 # -- General configuration ---------------------------------------------------
