009d674c7e85f29971987f9e3ac4bc03b26b9a39
Darwin <wenda.zhang@confluxnetwork.org>
2022-11-17 16:13:00 +0800

    Bump version: 1.0.0-beta.15 → 1.0.0-beta.16

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = "Zhang Wenda"
 
 # The full version, including alpha/beta/rc tags
 # modify by bumpversion
-release = "1.0.0-beta.15"
+release = "1.0.0-beta.16"
 
 
 # -- General configuration ---------------------------------------------------
