f5aff2db08fac3c257336972182b57902744ddae
Darwin <wenda.zhang@confluxnetwork.org>
2022-12-19 15:03:41 +0800

    Bump version: 1.0.0-beta.17 → 1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = "Zhang Wenda"
 
 # The full version, including alpha/beta/rc tags
 # modify by bumpversion
-release = "1.0.0-beta.17"
+release = "1.0.0"
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ with open('./README.md') as readme:
 setup(
     # the name must match the folder name 'verysimplemodule'
     name="cfx-address",
-    version="1.0.0-beta.17", # edit using bumpversion
+    version="1.0.0", # edit using bumpversion
     author="The Conflux foundation",
     author_email="wangpan@conflux-chain.org",
     long_description_content_type='text/markdown',
