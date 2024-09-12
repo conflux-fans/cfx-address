52cf0282df4a43d03723d8def63284c7f54ce2e1
darwintree <17946284+darwintree@users.noreply.github.com>
2024-09-12 14:43:25 +0800

    Bump version: 1.0.1 → 1.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = "Zhang Wenda"
 
 # The full version, including alpha/beta/rc tags
 # modify by bumpversion
-release = "1.0.1"
+release = "1.1.0"
 
 
 # -- General configuration ---------------------------------------------------
